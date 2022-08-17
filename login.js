9ed8a983dd7998040beb5fc750d34f3bb11d8476
TFMahabub <rjmahabub543@gmail.com>
2022-08-17 17:47:35 +0600

    3rd video

--- login.js
+++ login.js
@@ -6,7 +6,8 @@ document.getElementById('btn-submit').addEventListener('click', function(){
   const passwordValue = document.getElementById('password-input').value;
   //compare-valid-information-
   if(emailValue === 'rjmahabub543@gmail.com' && passwordValue === '123456789'){
-    console.log('login successfull')
+    //onclick-to another page-
+    window.location.href = 'http://127.0.0.1:5500/bank-page.html';
   }
   else{
     console.log('please input valid information');
